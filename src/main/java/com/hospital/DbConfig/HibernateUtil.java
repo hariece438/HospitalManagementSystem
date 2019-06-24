baba08b4fa7a679eb225b41bbe7c89e3d29b119a
hariece438 <45600914+hariece438@users.noreply.github.com>
2019-06-24 22:51:17 +0530

    Update HibernateUtil.java

--- src/main/java/com/hospital/DbConfig/HibernateUtil.java
+++ src/main/java/com/hospital/DbConfig/HibernateUtil.java
@@ -16,6 +16,9 @@ import org.hibernate.SessionFactory;
  * Hibernate Utility class with a convenient method to get Session Factory
  * object.
  * @author salah
+ *
+ *
+ *
  */
 public class HibernateUtil {
 
